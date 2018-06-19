fc5e52bb81e654898d41252f1274aea4cb3de688
ASUS\dtdat <dtdat.lsc@gmail.com>
2018-06-19 22:52:21 +0700

    Bai 25

--- TdtuShop.Web/app/components/product_categories/productCategoryListController.js
+++ TdtuShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,17 +1,27 @@
-﻿
-(function (app) {
+﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
     productCategoryListController.$inject = ['$scope', 'apiService'];
 
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
-
+        $scope.page = 0;
+        $scope.pagesCount = 0;
         $scope.getProductCagories = getProductCagories;
 
-        function getProductCagories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+        function getProductCagories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 20
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
                 console.log('Load productcategory failed.');
             });
@@ -20,5 +30,3 @@
         $scope.getProductCagories();
     }
 })(angular.module('tdtushop.product_categories'));
-
-
